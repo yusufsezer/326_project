45f93be9213f17bdb642b86dc650755652ef5ba2
YusufSezer <ysezer@umass.edu>
2018-11-02 22:39:40 +0000

    Added data models for app

--- src/PWM/PlayWithMe/models.py
+++ src/PWM/PlayWithMe/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django import forms
 
@@ -5,20 +6,40 @@ class User(models.Model):
     """Model representing a User."""
 
     username = models.CharField(
-        max_length=20, help_text="Enter a book genre (e.g. Science Fiction)"
+        max_length=20,
+        help_text="The user's username"
     )
 
     password = models.CharField(
-        max_length=20, help_text="Enter your password"
+        max_length=20,
+        help_text="The user's password"
     )
 
-    sessions = models.ForeignKey("Session", on_delete=models.SET_NULL, null=True)
+    sessions = models.ForeignKey(
+        "Session",
+         on_delete=models.SET_NULL,
+         null=True,
+         related_name="sessions"
+    )
 
-    # sessions_owned = models.ForeignKey("Session", on_delete=models.SET_NULL, null=True)
+    sessions_owned = models.ForeignKey(
+        "Session",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="sessions_owned"
+    )
 
-    games = models.ForeignKey("Game", on_delete=models.SET_NULL, null=True)
+    games = models.ForeignKey(
+        "Game",
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
-    platforms = models.ForeignKey("Platform", on_delete=models.SET_NULL, null=True)
+    platforms = models.ForeignKey(
+        "Platform",
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         """String for representing the Model object."""
@@ -27,26 +48,134 @@ class User(models.Model):
 class Session(models.Model):
     """Model representing a Session."""
 
-    # TODO: IMPLEMENT THIS CLASS
+    id = models.CharField(
+        max_length=60,
+        help_text="ID to uniquely identify a session",
+        primary_key=True
+    )
+
+    users = models.ForeignKey(
+        "User",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="users"
+    )
+
+    owner = models.OneToOneField(
+        "User",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="owner"
+    )
+
+    games = models.ForeignKey(
+        "Game",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    platforms = models.ForeignKey(
+        "Platform",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    messages = models.ForeignKey(
+        "Message",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    location = models.CharField(
+        max_length=100,
+        help_text="The session's location",
+        default=""
+    )
+
+    online = models.BooleanField(default=True)
 
     def __str__(self):
         """String for representing the Model object."""
-        return ""
+        return self.id
 
 class Game(models.Model):
     """Model representing a Game."""
 
-    # TODO: IMPLEMENT THIS CLASS
+    title = models.CharField(
+        max_length=100,
+        help_text="The game's title",
+        default=""
+    )
+
+    platforms = models.ForeignKey(
+        "Platform",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    online = models.BooleanField(default=True)
+
+    description = models.CharField(
+        max_length=100,
+        help_text="The game's description",
+        default=""
+    )
+
+    link = models.CharField(
+        max_length=100,
+        help_text="A link to this game's website",
+        default=""
+    )
 
     def __str__(self):
         """String for representing the Model object."""
-        return ""
+        return self.title
 
 class Platform(models.Model):
     """Model representing a Platform."""
 
-    # TODO: IMPLEMENT THIS CLASS
+    name = models.CharField(
+        max_length=100,
+        help_text="The name of the platform (e.g. PlayStation 4)",
+        default=""
+    )
+
+    games = models.ForeignKey(
+        "Game",
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         """String for representing the Model object."""
-        return ""
+        return self.name
+
+class Message(models.Model):
+    """Model representing a Message."""
+
+    text = models.CharField(
+        max_length=100,
+        help_text="The text content of the message",
+        default=""
+    )
+
+    owner = models.OneToOneField(
+        "User",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    context = models.OneToOneField(
+        "Session",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    datetime = models.DateTimeField(
+        default=datetime.now,
+        blank=True
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"Message from: {self.owner} with text: {self.text}"
