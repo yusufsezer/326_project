cf8aabac8884ba13c8c465d53712d5f97fa4d297
Chriscbr <crybicki98@gmail.com>
2018-11-02 18:29:23 -0400

    Fix images on main page, change urls so links on main page work

--- src/PWM/PlayWithMe/urls.py
+++ src/PWM/PlayWithMe/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import RedirectView
 from PlayWithMe import views
 
 
+# urlpatterns = [
+#     path("", views.index, name="index"),
+# ]
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('admin/', admin.site.urls),
+    # path('home/', include('PlayWithMe.urls')),
+    # path('', RedirectView.as_view(url='/home/')),
+    path('', views.index, name='index'),
+    path('search/', views.search, name='find_group'),
+    path('results/', views.results),
+    path('chat/', views.chat, name='my_groups'),
+    path('post_session/', views.post_session, name='create_group'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
