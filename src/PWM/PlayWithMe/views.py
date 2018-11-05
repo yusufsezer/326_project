048902e695ddbd49ce1ee995c568448954c426d8
RudyPikulik <rpikulik@umass.edu>
2018-11-05 23:05:07 +0000

    Fixed signup page

--- src/PWM/PlayWithMe/views.py
+++ src/PWM/PlayWithMe/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -92,3 +94,19 @@ def post_session(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, "post_session_page.html", context=context)
+
+# Found this method here:
+# https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
